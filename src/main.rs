d426a4e8ca538c41fe42ce5519968a48731475ff
flucium <14249224+flucium@users.noreply.github.com>
2022-09-13 22:38:08 +0900

    add ./src/main.rs
    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Arg, Command};
+
+fn app() -> Command<'static> {
+    Command::new("fli")
+        .version("1.0.0")
+        .author("flucium")
+        .args(vec![
+            Arg::new("all")
+                .long("all")
+                .short('a')
+                .required(false)
+                .takes_value(false),
+            Arg::new("format")
+                .long("format")
+                .short('f')
+                .required(false)
+                .takes_value(true),
+            Arg::new("count")
+                .long("count")
+                .short('c')
+                .required(false)
+                .takes_value(false),
+        ])
 }
+
+fn encode(mut string: String) {}
+
+fn main() {}
